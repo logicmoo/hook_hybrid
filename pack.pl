94865612d284c7d1be69e5b3ae97150d2aeb712e
root <logicmoo@gmail.com>
2017-03-09 15:05:17 -0800

    v1.1.112

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name(hook_hybrid).
-version('1.1.111').
+version('1.1.112').
 title('Hook assert retract call of *specific* predicates').
 keywords([term_expansion,database,utility]).
 
@@ -9,7 +9,10 @@ packager('TeamSPoon', 'https://github.com/TeamSPoon/').
 maintainer('TeamSPoon', 'https://github.com/TeamSPoon/').
 home('https://github.com/TeamSPoon/prologmud.git').
 download( 'https://github.com/TeamSPoon/prologmud/release/*.zip').
-
 requires(must_trace).
+requires(loop_check).
 requires(subclause_expansion).
+requires(clause_attvars).
+requires(file_scope).
+requires(each_call_cleanup).
 autoload(true).
