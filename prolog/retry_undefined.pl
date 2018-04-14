56850c8d20756ca850aa17e46d6a6b12a96a111b
Douglas R. Miles <logicmoo@gmail.com>
2018-04-14 03:31:47 -0700

    Sat Apr 14 03:31:42 PDT 2018

--- prolog/retry_undefined.pl
+++ prolog/retry_undefined.pl
@@ -100,6 +100,8 @@ uses_predicate(_DEF,_,Module,Name,Arity,Action) :-
 	'$autoload'(Module, Name, Arity), !,
 	Action = retry.
 
+uses_predicate(Setting,SM,M,F,A,Act):- dmsg(uses_predicate(Setting,SM,M,F,A,Act)),!,fail.
+
 uses_predicate(_,SM,M,F,A,Act):- ru:retry_undefined_hook(M,Setting),!, uses_predicate_setting(Setting,SM,M,F,A,Act).
 
 uses_predicate(_,SM,M,F,A,Act):- module_property(M,class(What)),
